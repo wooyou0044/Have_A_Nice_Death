612616ec2ba0948b57f32c725565241dff226229
jonghyun109 <mimj2527@gmail.com>
2025-01-12 14:46:58 +0900

    [Feat] FollowCamera 추가
    1. 배경 메인 카메라 따라가기
    2. 플레이어 중심으로 카메라 설정
    3. Woman_ghost 좌우 거리 움직임

--- Assets/Scripts/JongHyun/GhostWomanAI.cs
+++ Assets/Scripts/JongHyun/GhostWomanAI.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class GhostWomanAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public Animator animator;
+    public float moveSpeed = 0.01f;
+    public float moveDistance = 1f;
+    private Vector2 startPos;
+    private bool movingRight = true;
+
     void Start()
     {
-        
+        startPos = transform.position;
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();       
+        WomanAnimation();
     }
+    void Move()
+    {
+        if (movingRight)
+        {
+            HandleRotation();
+            transform.position += new Vector3(moveSpeed*Time.deltaTime, 0, 0);
+            if (transform.position.x > startPos.x + moveDistance)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            HandleRotation();
+            transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            if (transform.position.x < startPos.x - moveDistance)
+            {
+                movingRight = true;
+            }
+        }
+    }
+    private void WomanAnimation()
+    {
+        Vector2 movement = movingRight ? Vector2.right : Vector2.left;
+        if (animator != null)
+        {
+            animator.SetBool("IsWalk", true);
+        }
+    }
+    private void HandleRotation()
+    {
+        if (movingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);            
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);            
+        }
+    }
+    
 }
