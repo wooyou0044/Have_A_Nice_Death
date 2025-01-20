5dd5a8a942538bd7af804f92f1108c88cadc104d
wooyou0044 <chawoo2206@gmail.com>
2025-01-20 13:56:32 +0900

    [Feat] Boss UI 대화 창 애니메이션 추가

---
+++ Assets/Scripts/YounWoo/Boss/UI/EnterSpot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterSpot : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+
+    // 플레이어가 들어오면 Canvas가 SetActive됨
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("들어옴");
+        }
+    }
+}
