7364c37d980ec85de6894f9b5c2c20356dd3081b
hyugowacha <uena0506@gmail.com>
2025-01-21 19:44:18 +0900

    [Feat] 아이템 프리펩, 아이템 정보창 추가
    anima와 coffee의 프리펩, coffee의 아이템 상호작용 정보창 추가

---
+++ Assets/Scripts/EunA/Anima.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Anima : MonoBehaviour
+{
+    Collider2D animaCollider;
+    public GameObject animaImage;
+    public GameObject getItemEffect;
+
+    Player player;
+    float Destroytime;
+    bool isGetAnima = false;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (isGetAnima == true)
+        {
+            Destroytime += Time.deltaTime;
+
+            if (Destroytime >= 1.0f)
+            {
+                Destroy(gameObject);
+            }
+        }        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {        
+        player = GetComponent<Player>();
+
+        if (collision.CompareTag("Player"))
+        {
+            animaImage.SetActive(false);
+            getItemEffect.SetActive(true);
+
+            //player.Heal(false);
+
+            isGetAnima = true;
+        }     
+    }
+}
