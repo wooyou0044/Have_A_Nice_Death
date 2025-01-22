6cedb886e198e58acd16084ffaeac2bf3e5dd8c7
jonghyun109 <mimj2527@gmail.com>
2025-01-22 17:54:34 +0900

    [Fix] StateCanvas 덮어쓰기

---
+++ Assets/Scripts/JongHyun/ClearCanvas.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearCanvas : MonoBehaviour
+{
+    [SerializeField,Header("Panel")]
+    Image clearPanel;
+   
+    [SerializeField, Header("Image")]
+    Image clearImage;
+    
+    private void Start()
+    {
+    }    
+}
