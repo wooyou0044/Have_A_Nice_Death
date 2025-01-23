8ebbdfbc0b3dce8b54d8d150ba9019428c42eb59
jonghyun109 <mimj2527@gmail.com>
2025-01-23 14:01:02 +0900

    [Feat] 맵수정

--- Assets/Scripts/YoungHan/Managers/GameManager.cs
+++ Assets/Scripts/YoungHan/Managers/GameManager.cs
@@ -45,7 +45,7 @@ public sealed class GameManager : Manager<GameManager>
     private StatusPanel _statusPanel;
     [SerializeField]
     private GameObject _gameOverObject;
-
+    
 
     private List<IHittable> _hittableList = new List<IHittable>();
     private List<Ladder> _ladderList = new List<Ladder>();
@@ -237,14 +237,17 @@ public sealed class GameManager : Manager<GameManager>
                     instance.getObjectPooler.ShowEffect(monoBehaviour.gameObject, hittable.GetCollider2D().bounds.center, null);
                 }
             }
+            else if (instance._controller._player == (Object)hittable)
+            {
+                if (instance._gameOverObject != null)
+                {
+                    instance._gameOverObject.SetActive(true);
+                }
+            }
         }
         if (instance._controller._player == (Object)hittable)
         {
             instance._statusPanel?.Set(instance._controller._player);
-            if (instance._gameOverObject != null)
-            {
-                instance._gameOverObject.SetActive(true);
-            }
         }
     }
 
