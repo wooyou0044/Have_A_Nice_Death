cdb4151b6eaa17809b32fc60a33c899bbbe85431
jungyounghan <superbrush@naver.com>
2025-01-23 20:32:04 +0900

    [Test] 선을 감지하는 테스트 코드 추가

--- Assets/Scripts/YoungHan/YoungHan.cs
+++ Assets/Scripts/YoungHan/YoungHan.cs
@@ -1,53 +1,76 @@
 using UnityEngine;
 
 [DisallowMultipleComponent]
-[RequireComponent(typeof(Collider2D))]
-public class YoungHan : MonoBehaviour, IHittable, ILootable
+public class YoungHan : MonoBehaviour
 {
     [SerializeField]
-    private MonoBehaviour _prefab;
+    private int SegmentCount = 32;
 
-    private byte life = 1;
+    [SerializeField]
+    private Vector2 size;
 
-    public bool isAlive
+    [SerializeField]
+    private Vector2 offset;
+
+    [SerializeField]
+    private CapsuleDirection2D capsuleDirection2D;
+
+    private void OnDrawGizmos()
     {
-        get
+#if UNITY_EDITOR
+        Vector2[] vertices = GetVertices();
+        int length = vertices != null ? vertices.Length : 0;
+        for (int i = 0; i < length - 1; i++)
         {
-            return life > 0;
+            Debug.DrawLine(vertices[i], vertices[i + 1], Color.red);
         }
+#endif
     }
 
-    private bool _hasCollider2D = false;
-
-    private Collider2D _collider2D = null;
-
-    private Collider2D getCollider2D
+    public Vector2[] GetVertices()
     {
-        get
+        Vector2[] vertices = null;
+        if (transform != null)
         {
-            if (_hasCollider2D == false)
+            switch (capsuleDirection2D)
             {
-                _hasCollider2D = true;
-                _collider2D = GetComponent<Collider2D>();
+                case CapsuleDirection2D.Vertical:
+                    vertices = new Vector2[((SegmentCount + 1) * 2) + 1];
+                    float height = Mathf.Clamp(size.y - size.x, 0, size.y);
+                    vertices[0] = transform.TransformPoint(new Vector2(size.x * 0.5f * Mathf.Cos(Mathf.PI), height * 0.5f + (size.x * 0.5f) * Mathf.Sin(Mathf.PI)) + offset);
+                    // 아래쪽 반원
+                    for (int i = 0; i <= SegmentCount; i++)
+                    {
+                        float angle = Mathf.PI + Mathf.PI * i / SegmentCount;
+                        vertices[i + 1] = transform.TransformPoint(new Vector2(size.x * 0.5f * Mathf.Cos(angle), -height * 0.5f + (size.x * 0.5f) * Mathf.Sin(angle)) + offset);
+                    }
+                    // 위쪽 반원
+                    for (int i = 0; i <= SegmentCount; i++)
+                    {
+                        float angle = Mathf.PI * i / SegmentCount;
+                        vertices[i + SegmentCount + 2] = transform.TransformPoint(new Vector2(size.x * 0.5f * Mathf.Cos(angle), height * 0.5f + (size.x * 0.5f) * Mathf.Sin(angle)) + offset);
+                    }
+                    break;
+                case CapsuleDirection2D.Horizontal:
+                    vertices = new Vector2[((SegmentCount + 1) * 2) + 1];
+                    float width = Mathf.Clamp(size.x - size.y, 0, size.x);
+                    vertices[0] = transform.TransformPoint(new Vector2(width * 0.5f + (size.y * 0.5f) * Mathf.Cos(-Mathf.PI * 0.5f + Mathf.PI), size.y * 0.5f * Mathf.Sin(-Mathf.PI * 0.5f + Mathf.PI)) + offset);
+                    // 왼쪽 반원
+                    for (int i = 0; i <= SegmentCount; i++)
+                    {
+                        float angle = Mathf.PI * 0.5f + Mathf.PI * i / SegmentCount;
+                        vertices[i + 1] = transform.TransformPoint(new Vector2(-width * 0.5f + (size.y * 0.5f) * Mathf.Cos(angle), size.y * 0.5f * Mathf.Sin(angle)) + offset);
+                    }
+                    // 오른쪽 반원
+                    for (int i = 0; i <= SegmentCount; i++)
+                    {
+                        float angle = -Mathf.PI * 0.5f + Mathf.PI * i / SegmentCount;
+                        vertices[i + SegmentCount + 2] = transform.TransformPoint(new Vector2(width * 0.5f + (size.y * 0.5f) * Mathf.Cos(angle), size.y * 0.5f * Mathf.Sin(angle)) + offset);
+                    }
+                    break;
             }
-            return _collider2D;
         }
-    }
-    public void Hit(Strike strike)
-    {
-        life = 0;
-        gameObject.SetActive(false);
-        GameManager.Report(this, strike.result);
-    }
-
-    public Collider2D GetCollider2D()
-    {
-        return getCollider2D;
-    }
-
-    public MonoBehaviour GetLootObject()
-    {
-        return _prefab;
+        return vertices;
     }
 
 }
